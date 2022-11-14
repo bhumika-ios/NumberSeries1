a6255fc2871cfd5d21bc6411a531b3e61ca3bc66
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-11-14 15:57:20 +0530

    Initial Commit

---
+++ NumberSeries/NumberSeriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NumberSeriesApp.swift
+//  NumberSeries
+//
+//  Created by Bhumika Patel on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct NumberSeriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
